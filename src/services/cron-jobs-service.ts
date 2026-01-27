8cbb0a4420943ecbe87aa044ffb84f76a31b0c46
Jon Arbell De Ocampo <deocampo.jonarbell@primex.ventures>
2026-01-27 16:13:07 +0800

    update

--- src/services/cron-jobs-service.ts
+++ src/services/cron-jobs-service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const jobs = async () => {
-  const base = process.env.BASE_LOCAL_URL;
+  const base = process.env.BASE_PROD_URL;
 
   try {
     await axios.get(`${base}`);
